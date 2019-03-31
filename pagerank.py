b5c2e07c1024dec907901b558c2baffe0da031c3
한지승 <jinh574@naver.com>
2019-03-31 12:58:18 +0900

    Edit pagerank calculate method

--- pagerank.py
+++ pagerank.py
@@ -17,31 +17,28 @@ class PageRank(object):
         self.id2idx = {v: idx for idx, v in enumerate(self.doc_ids)}
 
     def __call__(self):
-        # back_link = self._linkdb_to_dict(self.db.get_back_link())
         forward_link = self._linkdb_to_dict(self.db.get_forward_link())
         doc_size = len(self.doc_ids)
 
-        link_matrix = np.zeros((doc_size, doc_size), dtype=np.float32)
-        scores = np.ones(doc_size, dtype=np.float32) / doc_size
+        link_matrix = np.ones((doc_size, doc_size),
+                              dtype=np.float32) * (self.jump_prob / doc_size)
 
+        scores = np.ones(doc_size, dtype=np.float32) / doc_size
         for i in range(doc_size):
             doc_id = self.doc_ids[i]
             if doc_id in forward_link.keys():
                 links = list(
                     map(lambda x: self.id2idx[x], forward_link[doc_id]))
-                links.append(self.id2idx[doc_id])
-                link_matrix[i, links] = 1 / len(links)
+                link_matrix[i, links] += (1 - self.jump_prob) / len(links)
             else:
-                link_matrix[i, self.id2idx[doc_id]] = 1
-
+                link_matrix[i, :] += (1 - self.jump_prob) / doc_size
         link_matrix = np.transpose(link_matrix)
         scores = np.transpose(scores)
 
         delta = 1
         while delta > self.e:
             prev = np.sum(scores)
-            scores = (self.jump_prob / doc_size) + \
-                (1 - self.jump_prob) * np.matmul(link_matrix, scores)
+            scores = np.matmul(link_matrix, scores)
             delta = prev - np.sum(scores)
 
         return list(zip(self.doc_ids, scores.tolist()))
